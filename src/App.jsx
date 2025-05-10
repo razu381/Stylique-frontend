00866b6758753361444d16c1478254123988fa2b
razu381 <sirazu52@gmail.com>
2025-05-10 22:24:05 +0600

    Frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-red-700 text-center py-20">
+        Vite + React
+      </h1>
+    </>
+  );
+}
+
+export default App;
